1e093bd05229ab81b934622a8192841e8257732b
Navraj Saini <navraj.saini@hotmail.com>
2019-03-24 11:22:58 -0600

    added in another way to get the info

--- beautifulsoup_requests.py
+++ beautifulsoup_requests.py
@@ -5,11 +5,13 @@ import time #for the timer
 def timer():
     query = input ("What news query would you like to search for on BBC? ")
     request(query)
+    print('waiting a few seconds before the next prompt')
+    time.sleep(10)#10 sec delay just in case
 
 
 def request(input):
 
-    print ('searching for: ' + input)
+    print ('Searching for: ' + input)
 
     #request the info the user wants from BBC
     response = requests.get("https://www.bbc.co.uk/search?q="+input+
@@ -18,22 +20,34 @@ def request(input):
     #use BeautifulSoup to get the edit the text
     soup_obj = bs4.BeautifulSoup(response.text,'lxml')
 
-    print(soup_obj.select('title')[0].getText())#print the title of the page
+    print ('Title: '+ soup_obj.select('title')[0].getText())
+    print()#print the title of the page and then a new line
 
     #get all the h1 tags and print them
+    #this get all the titles with the h1 tag
     for anchor in soup_obj.findAll('h1'):
         print(anchor.string)
+
+    print()
+    print ('Another way to do this: ')
+    print()
+
+    #this finds the actual headline itemprop and will print it
+    for anchor in soup_obj.findAll(itemprop = 'headline'):
+        print (anchor.string)
+
 
 def main():
     cont = True
     while cont == True:
         timer()
-        inp = input('would you like to search again? (enter y or n)')
-        time.sleep(10)#delay of 10 seconds just in case there's a request delay
+        inp = input('would you like to search again? (enter y or n) ')
         if inp == 'y':
             cont = True
         elif inp == 'n':
-            cont= False
+            cont = False
+            print ('exiting...')
+
 
 if __name__ == "__main__":
     main()
