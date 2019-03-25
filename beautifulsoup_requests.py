bac7df5b8db97b1699c7cb8c2226ee168aa41d8f
Navraj Saini <navraj.saini@hotmail.com>
2019-03-25 09:10:22 -0600

    added in the database. need to make sure mongod opens properly.

--- beautifulsoup_requests.py
+++ beautifulsoup_requests.py
@@ -1,7 +1,11 @@
 import requests #for the website
 import bs4 #to make it easier to view the data
 import time #for the timer
-import pymongo #for the database
+from pymongo import MongoClient#for the database
+
+client = MongoClient()
+db = client['BBC']
+#collection = db['Headlines']
 
 def timer():
     query = input ("What news query would you like to search for on BBC? ")
@@ -11,11 +15,7 @@ def timer():
 
 
 def request(input):
-    #create the database
-    mydatabase = pymongo.MongoClient('mongodb://localhost:27017/')
-    mydb = mydatabase['mydbase']
-    mycol = mydb['headlines']
-
+    posts = db.posts # for inserting into the DB
     print ('Searching for: ' + input)
 
     #request the info the user wants from BBC
@@ -36,17 +36,18 @@ def request(input):
     print()
     print ('Another way to do this: ')
     print()
+    line = {'Title of the webpage: ': soup_obj.select('title')[0].getText()}
+    posts.insert_one(line)
 
     #this finds the actual headline itemprop and will print it
     for anchor in soup_obj.findAll(itemprop = 'headline'):
         print (anchor.string)
-        mydict = {"Headline": anchor.string}
-        x = mycol.insert_one(mydict)
+        line = {"Title": anchor.string}
+        posts.insert_one(line)
+
 
 
 def main():
-
-
     cont = True
     while cont == True:
         timer()
@@ -55,6 +56,7 @@ def main():
             cont = True
         elif inp == 'n':
             cont = False
+            #add in code to view the database
             print ('exiting...')
 
 
