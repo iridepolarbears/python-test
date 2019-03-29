111ed0688a78ae6aec44d34313e0d8243a7d3d7e
Navraj Saini <navraj.saini@hotmail.com>
2019-03-28 18:29:27 -0600

    fixed bugs, now it prints the title and the intro.

--- beautifulsoup_requests.py
+++ beautifulsoup_requests.py
@@ -2,7 +2,6 @@ import requests #for the website
 import bs4 #to make it easier to view the data
 import time #for the timer
 from pymongo import MongoClient#for the database
-from urllib.request import urlopen #library for opening links
 
 client = MongoClient()
 db = client['BBC']
@@ -29,16 +28,8 @@ def requestbbc(input):
     print ('Title: '+ soup_obj.select('title')[0].getText())
     print()#print the title of the page and then a new line
 
-    #get all the h1 tags and print them
-    #this get all the titles with the h1 tag
-    for anchor in soup_obj.findAll('h1'):
-        print(anchor.string)
-
-    print()
-    print ('Another way to do this: ')
-    print()
     line = {'Title': soup_obj.select('title')[0].getText()}
-    #result = headlines.insert_one(line)
+    headlines.insert_one(line)
 
     #this finds the actual headline itemprop and will print it
     #also push it into the database
@@ -48,24 +39,29 @@ def requestbbc(input):
         link = a_tag['href']#get the actual link
         #get the info from the url
         headline_in = headline_info(link)#get the headline info from the link
-        print (anchor.string + ': ' + headline_in)
-        line = {
-        "Title": anchor.string,
-        "info" : headline_in
-        }
-        result = headlines.insert_one(line)
+        if headline_in is not 'no':
+            print (anchor.string + ': ' + headline_in)
+            print()
+            line = {
+                    "Title": anchor.string + ': ' + headline_in
+                    }
+            result = headlines.insert_one(line)
 
 def headline_info (link):
     response = requests.get(link)
     web = bs4.BeautifulSoup(response.text,'lxml')
-    for anchor in web.find('p', {'class' : 'story-body__introduction'}):
+    anchor = web.find('p', {'class' : 'story-body__introduction'})
+    if anchor is not None:
         return anchor.string
+    else:
+        return 'no'
 
 
 def printdb():
     #add in code to view the database
     for headline in headlines.find():
-        print (headline['Title'] + ': ' + headline['info'])
+        print (headline['Title'])
+        print()
 
 
 def main():
@@ -77,11 +73,9 @@ def main():
             cont = True
         elif inp == 'n':
             cont = False
-            data = input('To print the database enter y: ')
-            if data == 'y':
-                print ('printing the current database: ')
-                print()
-                printdb()
+            print ('printing the current database: ')
+            print()
+            printdb()
             print ('exiting...')
             exit()
 
